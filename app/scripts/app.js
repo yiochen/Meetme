408550baa4f192e92e094959162ee763136b4866
Yiou Chen <chen.yiou1992@gmail.com>
2015-05-25 23:35:05 -0400

    implemented simple-login-page. all logic work

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,35 +6,45 @@ var app;
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   app= document.querySelector('#app');
-  app.appName = 'Yo, Polymer App!';
-  var fb=new Firebase("https://meetme.firebaseio.com");
+  app.appName = 'Meet at Time square';
+  app.fb=new Firebase("https://meetme.firebaseio.com");
+    console.log('register firebase');
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('template-bound', function() {
     console.log('Our app is ready to rock!');
-    var me={name:"yiou"};
+    var me={name:"me"};
+    app.me=me;
     var map=document.querySelector('#map');
+    app.map=map;
     var pin=document.querySelector('#pin');
-    var app=document.querySelector('#app');
+    app.pin=pin;
     pin.markers=[];
     navigator.geolocation.getCurrentPosition(function(pos){
         map.latitude=pos.coords.latitude;
         map.longitude=pos.coords.longitude;
-        fb.set({name:"yiou",lat:map.latitude,lng:map.longitude});
-        map.zoom=15;
         me.lat=pos.coords.latitude;
         me.lng=pos.coords.longitude;
-        geo_success(pos);
+        map.zoom=15;
         pin.markers[0]=me; 
         var watchID=navigator.geolocation.watchPosition(geo_success,geo_error);
     },geo_error);
+    
     function geo_success(pos){
         me.lat=pos.coords.latitude;
         me.lng=pos.coords.longitude;
+        if (app.user){
+            console.log('updating my location');
+            var fbchild=app.fb.child(app.user.uid);
+            fbchild.update({lat:me.lat,lng:me.lng});
+        }
     }
+      
     function geo_error(){
         alert("sorry, no position available");
     }
+    
+        
     app.openOverlay = function(e){
         app.placeholder="Hello world"; 
     };
